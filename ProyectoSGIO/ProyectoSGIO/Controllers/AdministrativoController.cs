216153d477bbc72c8d7df64db3b6dcfbd3435f8c
XavierEstrada <xguadamuz70935@ufide.ac.cr>
2024-09-11 03:56:40 -0600

    Vista + js

---
+++ ProyectoSGIO/ProyectoSGIO/Controllers/AdministrativoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoSGIO.Controllers
+{
+    public class AdministrativoController : Controller
+    {
+        public ActionResult NewUserAdmin()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProyectoSGIO/ProyectoSGIO/root/js/funciones.js
@@ -0,0 +1,21 @@
+﻿// Función para generar una contraseña automáticamente
+document.getElementById('generar-contrasena').addEventListener('click', function () {
+    const contraseña = Math.random().toString(36).slice(-8);
+    document.getElementById('contraseña').value = contraseña;
+});
+
+// Función para mostrar/ocultar la contraseña
+document.getElementById('toggle-password').addEventListener('click', function () {
+    const contraseñaInput = document.getElementById('contraseña');
+    const icono = document.getElementById('icon-password');
+
+    if (contraseñaInput.type === "password") {
+        contraseñaInput.type = "text";
+        icono.classList.remove('bi-eye-slash');
+        icono.classList.add('bi-eye');
+    } else {
+        contraseñaInput.type = "password";
+        icono.classList.remove('bi-eye');
+        icono.classList.add('bi-eye-slash');
+    }
+});
