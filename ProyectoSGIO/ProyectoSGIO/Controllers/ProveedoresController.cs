97f0ad89f5bd53141091baa9bc6a26a472dd6053
XavierEstrada <xguadamuz70935@ufide.ac.cr>
2024-09-12 00:26:39 -0600

    CRUD Proveedores

---
+++ ProyectoSGIO/ProyectoSGIO/Controllers/ProveedoresController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoSGIO.Controllers
+{
+    public class ProveedoresController : Controller
+    {
+        public ActionResult RegistroProveedor()
+        {
+            return View();
+        }
+        public ActionResult EditarProveedor()
+        {
+            return View();
+        }
+        public ActionResult EliminarProveedor()
+        {
+            return View();
+        }
+        public ActionResult VisualizarProveedores()
+        {
+            return View();
+        }
+    }
+}
