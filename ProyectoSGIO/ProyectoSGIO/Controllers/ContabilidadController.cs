eaac82c08cb374974eed36917e7c224596ee365e
XavierEstrada <xguadamuz70935@ufide.ac.cr>
2024-09-13 21:05:13 -0600

    Contabilidad

---
+++ ProyectoSGIO/ProyectoSGIO/Controllers/ContabilidadController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoSGIO.Controllers
+{
+    public class ContabilidadController : Controller
+    {
+        public ActionResult RegistroFactura()
+        {
+            return View();
+        }
+        public ActionResult EditarFactura()
+        {
+            return View();
+        }
+        public ActionResult VisualizarFacturas()
+        {
+            return View();
+        }
+        public ActionResult DetalleFactura()
+        {
+            return View();
+        }
+
+        //Cierres Financieros
+        public ActionResult CierreFinancieroMes()
+        {
+            return View();
+        }
+        public ActionResult ConfirmarCierreMes()
+        {
+            return View();
+        }
+
+        //Anual
+        public ActionResult CierreFinancieroAnual()
+        {
+            return View();
+        }
+        public ActionResult ConfirmarCierreAnual()
+        {
+            return View();
+        }
+        public ActionResult ReconciliacionCuentas()
+        {
+            return View();
+        }
+    }
+}
