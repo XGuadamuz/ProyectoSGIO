6f9940712d9263ad3a828b9244fd43185a3f78f2
GERARDO MORA <gmora60255@ufide.ac.cr>
2024-09-25 19:57:46 -0600

    Se agrega controlador de Home

---
+++ ProyectoSGIO/ProyectoSGIOCore/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoSGIOCore.Controllers
+{
+    [ResponseCache(Duration = 0, NoStore = true)]
+    public class HomeController() : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Portafolio()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult PortafolioItem()
+        {
+            return View();
+        }
+
+    }
+}
