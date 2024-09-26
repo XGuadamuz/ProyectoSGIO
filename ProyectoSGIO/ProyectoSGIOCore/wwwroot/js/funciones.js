9609f2f65e226670094953b35fbcde5b9c386fab
gerarmoca2210 <gerarmoca@gmail.com>
2024-09-25 19:02:28 -0600

    Inicio Migra a Core

---
+++ ProyectoSGIO/ProyectoSGIOCore/wwwroot/js/funciones.js
@@ -0,0 +1,32 @@
+﻿// Función para generar una contraseña automáticamente
+document.getElementById('generar-contrasena').addEventListener('click', function () {
+    const contraseña = Math.random().toString(36).slice(-8);
+    document.getElementById('contraseña').value = contraseña;
+});
+
+// Función para mostrar/ocultar la contraseña
+document.getElementById('toggle-password').addEventListener('click', function () {
+    const contraseñaInput = document.getElementById('contraseña');
+    const icono = document.getElementById('icon-password');
+
+    if (contraseñaInput.type === "password") {
+        contraseñaInput.type = "text";
+        icono.classList.remove('bi-eye-slash');
+        icono.classList.add('bi-eye');
+    } else {
+        contraseñaInput.type = "password";
+        icono.classList.remove('bi-eye');
+        icono.classList.add('bi-eye-slash');
+    }
+});
+
+// Función para habilitar la edición del correo
+document.getElementById('edit-correo').addEventListener('click', function () {
+    const correoInput = document.getElementById('correo');
+    if (correoInput.disabled) {
+        correoInput.disabled = false;
+        correoInput.focus();
+    } else {
+        correoInput.disabled = true;
+    }
+});
