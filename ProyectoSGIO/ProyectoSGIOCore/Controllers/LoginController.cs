6690be33dace8695e28db41a64f04490800cacd4
XavierEstrada <xguadamuz70935@ufide.ac.cr>
2024-09-28 16:57:16 -0600

    Registro + Alerta

---
+++ ProyectoSGIO/ProyectoSGIOCore/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoSGIOCore.Models;
+
+namespace ProyectoSGIOCore.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registro(LoginModel login)
+        {
+            if (ModelState.IsValid)
+            {
+                // Falta lógica para crear el usuario y guardarlo en la base de datos
+                // Redirigir a una vista de éxito o al inicio de sesión
+                TempData["SuccessMessage"] = "Usuario creado exitosamente.";
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View(login);
+            }
+        }
+    }
+}
---
+++ ProyectoSGIO/ProyectoSGIOCore/Models/LoginModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoSGIOCore.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido es requerido")]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "El correo es requerido")]
+        [EmailAddress(ErrorMessage = "El formato del correo es inválido")]
+        public string Correo { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        [MinLength(6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres")]
+        public string Contraseña { get; set; }
+
+        [Required(ErrorMessage = "Debe confirmar la contraseña")]
+        [Compare("Contraseña", ErrorMessage = "Las contraseñas no coinciden")]
+        public string ConfirmarContraseña { get; set; }
+    }
+}
