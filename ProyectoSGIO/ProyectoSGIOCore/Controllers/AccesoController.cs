a987e4f4886f76bfa2a41f77e68afc87c01a8c4e
XavierEstrada <xguadamuz70935@ufide.ac.cr>
2024-10-09 23:14:06 -0600

    Refinamientos

--- ProyectoSGIO/ProyectoSGIOCore/Controllers/AccesoController.cs
+++ ProyectoSGIO/ProyectoSGIOCore/Controllers/AccesoController.cs
@@ -51,7 +51,8 @@ namespace ProyectoSGIOCore.Controllers
                 Apellido = modelo.Apellido,
                 Correo = modelo.Correo,
                 Clave = modelo.Clave,
-                IdRol = rolDefault.IdRol
+                IdRol = rolDefault.IdRol,
+                Activo = true
             };
 
             await _dbContext.Usuarios.AddAsync(usuario);
