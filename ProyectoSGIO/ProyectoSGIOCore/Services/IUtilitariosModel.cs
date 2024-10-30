94a758bdf510ca434dfe5eea2656f48147363a7c
GERARDO MORA <gmora60255@ufide.ac.cr>
2024-10-30 09:19:59 -0600

    Cambiar Contraseña

---
+++ ProyectoSGIO/ProyectoSGIOCore/Services/IUtilitariosModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ProyectoSGIOCore.Services
+{
+    public interface IUtilitariosModel
+    {
+        public string GenerarToken(long codigo_usuario);
+        public string Encrypt(string texto);
+        public string Decrypt(string texto);
+        public string GenerarCodigo();
+        public void EnviarCorreo(string Destinatario, string Asunto, string Mensaje);
+    }
+}
