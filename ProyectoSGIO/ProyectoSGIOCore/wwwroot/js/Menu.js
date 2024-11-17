7d03762201a5b5935d5ed995d0727ad193a69533
XavierEstrada <xguadamuz70935@ufide.ac.cr>
2024-11-16 22:05:46 -0600

    Menu lateral

---
+++ ProyectoSGIO/ProyectoSGIOCore/wwwroot/js/Menu.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll("#sidebar .nav-link");
+    links.forEach(link => {
+        link.addEventListener("click", () => {
+            links.forEach(link => link.classList.remove("active"));
+            link.classList.add("active");
+        });
+    });
+});
