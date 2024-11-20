e8f8dd1c737cf3c3f1ce1f4157fbf43043c478d6
GERARDO MORA <gmora60255@ufide.ac.cr>
2024-11-20 13:34:37 -0600

    Se agregan Hitos

---
+++ ProyectoSGIO/ProyectoSGIOCore/Migrations/20241120153508_Hitos Usuario.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoSGIOCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class HitosUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Hitos_Empleado_ResponsableIdEmpleado",
+                table: "Hitos");
+
+            migrationBuilder.RenameColumn(
+                name: "ResponsableIdEmpleado",
+                table: "Hitos",
+                newName: "UsuarioIdUsuario");
+
+            migrationBuilder.RenameColumn(
+                name: "IdEmpleado",
+                table: "Hitos",
+                newName: "IdUsuario");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Hitos_ResponsableIdEmpleado",
+                table: "Hitos",
+                newName: "IX_Hitos_UsuarioIdUsuario");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Hitos_Usuario_UsuarioIdUsuario",
+                table: "Hitos",
+                column: "UsuarioIdUsuario",
+                principalTable: "Usuario",
+                principalColumn: "IdUsuario");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Hitos_Usuario_UsuarioIdUsuario",
+                table: "Hitos");
+
+            migrationBuilder.RenameColumn(
+                name: "UsuarioIdUsuario",
+                table: "Hitos",
+                newName: "ResponsableIdEmpleado");
+
+            migrationBuilder.RenameColumn(
+                name: "IdUsuario",
+                table: "Hitos",
+                newName: "IdEmpleado");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Hitos_UsuarioIdUsuario",
+                table: "Hitos",
+                newName: "IX_Hitos_ResponsableIdEmpleado");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Hitos_Empleado_ResponsableIdEmpleado",
+                table: "Hitos",
+                column: "ResponsableIdEmpleado",
+                principalTable: "Empleado",
+                principalColumn: "IdEmpleado");
+        }
+    }
+}
